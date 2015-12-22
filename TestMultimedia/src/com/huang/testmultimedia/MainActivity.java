4f289498bd1a1e1f62d87f7b0d5d8ebc1d6e09c5
huangzhiyang <hzhiyang84@163.com>
2015-12-22 18:32:54 +0800

    1.更新

--- TestMultimedia/src/com/huang/testmultimedia/MainActivity.java
+++ TestMultimedia/src/com/huang/testmultimedia/MainActivity.java
@@ -1,15 +1,22 @@
 package com.huang.testmultimedia;
 
+import com.huang.testmultimedia.R.id;
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
 
 public class MainActivity extends Activity {
 
+	private static final String TAG = "TestMultiMedia";
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.main_activity);
 	}
 
 	@Override
@@ -18,5 +25,30 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
+	
+	public class MyOnClickListener implements OnClickListener{
 
+		@Override
+		public void onClick(View v) {
+			// TODO Auto-generated method stub
+			switch(v.getId()){
+			case R.id.imageButton_music:
+				break;
+			case R.id.imageButton_photo:
+				break;
+			case R.id.imageButton_video:
+				break;
+			}
+		}
+	}
+
+	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+		// TODO Auto-generated method stub
+		Log.d(TAG,"kecode:" + keyCode + "key event"+event);
+		return super.onKeyDown(keyCode, event);
+	};
+	
+	
+	
 }
